0ec85541d5aaa594409b77512b92ab4b4dc59f29
CommunistPancake <communist.pancake@gmx.fr>
2013-11-07 17:35:01 -0500

    'init'

---
+++ gamemode/shared.lua
@@ -0,0 +1,10 @@
+GM.Name    = "Science and Industry"
+GM.Author  = "cash4ads"
+GM.Email   = "contact@cash4ads.biz"
+GM.Website = "cash4ads.biz"
+
+DeriveGamemode("sandbox")
+
+TEAM_AFD = team.SetUp(1, "Amalgamated Fluorodynamics", Color(35, 83, 119, 255))
+TEAM_MCL = team.SetUp(2, "Midland Carbide Labs", Color(63, 110, 15, 255))
+TEAM_SPECTATOR = team.SetUp(3, "Spectator", Color(230, 230, 230, 255))
